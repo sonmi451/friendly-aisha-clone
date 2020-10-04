9ab3b332d48cd0209fe2f4378bff277bdadd45c3
Aisha <aishamclean451@gmail.com>
2020-10-04 02:02:56 +0100

    add new movie scheduling commands

--- msg-read-reply.py
+++ msg-read-reply.py
@@ -2,6 +2,9 @@ import os
 import random
 
 import discord
+import requests
+
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -11,8 +14,32 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 BOT_USER_ID = os.getenv('BOT_USER_ID')
 BOT_ROLE_ID = os.getenv('BOT_ROLE_ID')
+ADGENDA = os.getenv('GOOGLE_CALENDER')
 
 DEBUG = True
+
+client = commands.Bot(command_prefix='a!')
+
+
+def scrape_events_from_calender():
+    adgenda_html = requests.get(ADGENDA)
+    soup = BeautifulSoup(adgenda_html.text, 'html.parser')
+    adgenda_events = soup.select("body > div.view-container-border > div > div")
+    events = []
+    for event in adgenda_events:
+        event_text = event.text
+        oneline_event_text = event_text.replace('\n', ' ').replace('\r', '')
+        events.append(oneline_event_text)
+    return events
+
+
+def format_full_schedule(schedule):
+    return ',\n'.join(schedule)
+
+
+def next_movie(schedule):
+    return schedule[0]
+
 
 def get_random_friendly_advice():
     with open('friendly_robot_advice.txt') as f:
@@ -20,7 +47,6 @@ def get_random_friendly_advice():
     random_friendly_message=random.choice(friendly_robot_advice)
     return random_friendly_message
 
-client = commands.Bot(command_prefix='a!')
 
 @client.event
 async def on_ready():
@@ -33,9 +59,8 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, it\'s nice to see you!'
-    )
+    await member.dm_channel.send(f'Hi {member.name}, it\'s nice to see you!')
+
 
 @client.event
 async def on_message(message):
@@ -51,5 +76,20 @@ async def on_message(message):
     if 'robot' in message.content:
         friendly_message = get_random_friendly_advice()
         await message.channel.send(friendly_message)
+    await client.process_commands(message)
+
+
+@client.command(name='movies', help='Read the full movie schedule from the calendar')
+async def full_schedule(ctx):
+    schedule = scrape_events_from_calender()
+    print_schedule = format_full_schedule(schedule)
+    await ctx.send(print_schedule)
+
+
+@client.command(name='movie', help='Reads the next scheduled movie schedule from the calendar')
+async def next_scheduled(ctx):
+    schedule = scrape_events_from_calender()
+    movie = next_movie(schedule)
+    await ctx.send(movie)
 
 client.run(TOKEN)
