446c6684e1105661c124847c6d8b63ac99bd9866
Aisha <aishamclean451@gmail.com>
2020-10-18 23:15:11 +0100

    more sophisticated scraper for movie calender

--- robot.py
+++ robot.py
@@ -20,6 +20,28 @@ DEBUG = True
 client = commands.Bot(command_prefix='a?')
 
 
+def scrape_movie_events_from_calender(calender):
+    events = []
+    adgenda_html = requests.get(calender)
+    soup = BeautifulSoup(adgenda_html.text, 'html.parser')
+    if not 'Nothing currently scheduled' in soup.text:
+        adgenda_events = soup.select("body > div.view-container-border > div > div")
+        for event in adgenda_events:
+            date = ''
+            event_date = event.find("div", class_="date")
+            if event_date:
+                date = event_date.text
+            event_times = event.findAll("td", class_="event-time")
+            if event_times:
+                times = [event_time.text for event_time in event_times]
+            event_summary = event.findAll("div", class_="event-summary")
+            if event_summary:
+                descriptions = [event.text for event in event_summary]
+            oneline_event_list = [date, dict(zip(times, descriptions))]
+            events.append(oneline_event_list)
+    return events
+
+
 def scrape_events_from_calender(calender):
     events = []
     adgenda_html = requests.get(calender)
@@ -34,32 +56,32 @@ def scrape_events_from_calender(calender):
 
 
 def embed_movie_schedule(schedule, first=False):
-    formattd_schedule = discord.Embed(title='Movie Schedule')
+    formatted_schedule = discord.Embed(title='Movie Schedule')
     if not schedule:
-        formattd_schedule.add_field(name='Empty schedule', value='Hmm, looks like nothing is scheduled!')
+        formatted_schedule.add_field(name='Empty schedule', value='Hmm, looks like nothing is scheduled!')
     else:
         if first:
             schedule = [schedule[0]]
 
-        for event in schedule:
-            formattd_schedule.add_field(name=event[0] + ' - ' + event[1],
-                                        value=event[2],
+        for day in schedule:
+            formatted_schedule.add_field(name=day[0],
+                                        value='\n'.join([str(time + ': ' + description) for time, description in day[1].items()]),
                                         inline=False)
 
-    formattd_schedule.add_field(name='Calender',
+    formatted_schedule.add_field(name='Calender',
                                 value='[See the full calender of events online](https://calendar.google.com/calendar/u/0/embed?src=qjva8eaked6q9vdcgqkspqvseg@group.calendar.google.com)',
                                 inline=False)
-    return formattd_schedule
+    return formatted_schedule
 
 
 def embed_games_schedule(schedule):
-    formattd_schedule = discord.Embed(title='TV Games')
-    formattd_schedule.add_field(name='Every Wednesday at 8PM!', value='join Acres Greg in the TV games voice channel for socialising and games')
-    for event in schedule:
-        formattd_schedule.add_field(name=event[0],
-                                    value='\n'.join(event[1:]),
+    formatted_schedule = discord.Embed(title='TV Games')
+    formatted_schedule.add_field(name='Every Wednesday at 8PM!', value='join Acres Greg in the TV games voice channel for socialising and games')
+    for day in schedule:
+        formatted_schedule.add_field(name=day[0],
+                                    value='\n'.join(day[1:]),
                                     inline=False)
-    return formattd_schedule
+    return formatted_schedule
 
 
 def embed_response(text):
@@ -107,7 +129,7 @@ async def on_message(message):
         await message.channel.send('Praise be the regulations')
 
     if 'movie schedule' in message.content:
-        schedule = scrape_events_from_calender(MOVIE_AGENDA)
+        schedule = scrape_movie_events_from_calender(MOVIE_AGENDA)
         print_schedule = embed_movie_schedule(schedule)
         await message.channel.send(embed=print_schedule)
 
@@ -121,14 +143,14 @@ async def on_message(message):
 
 @client.command(name='movies', help='Read the full movie schedule from the calendar')
 async def full_schedule(ctx):
-    schedule = scrape_events_from_calender(MOVIE_AGENDA)
+    schedule = scrape_movie_events_from_calender(MOVIE_AGENDA)
     print_schedule = embed_movie_schedule(schedule)
     await ctx.send(embed=print_schedule)
 
 
 @client.command(name='movie', help='Reads the next scheduled movie schedule from the calendar')
 async def next_scheduled(ctx):
-    schedule = scrape_events_from_calender(MOVIE_AGENDA)
+    schedule = scrape_movie_events_from_calender(MOVIE_AGENDA)
     print_schedule = embed_movie_schedule(schedule, first=True)
     await ctx.send(embed=print_schedule)
 
