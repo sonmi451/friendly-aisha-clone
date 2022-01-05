0c0fc1839dd61fd58b74e9afb40933c38d5a8879
sonmi451 <aishamclean451@gmail.com>
2022-01-05 12:45:19 +0000

    fiz daylight sayings problem

--- robot/calendars.py
+++ robot/calendars.py
@@ -1,14 +1,15 @@
+import os
+import time
+from datetime import datetime, timedelta, timezone
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta, timezone
-import pytz
 
 TIME_FORMAT = "%H:%M"
 
 def is_british_summer_time():
-    utc_now = datetime.now(timezone.utc)
-    time_in_london = utc_now.replace(tzinfo=timezone.utc).astimezone(tz=pytz.timezone("Europe/London"))
-    return time_in_london != utc_now
+    os.environ['TZ'] = 'Europe/London'
+    time.tzset()
+    return time.localtime().tm_isdst
 
 def scrape_events_from_calender(calender):
     events = []
@@ -24,10 +25,9 @@ def scrape_events_from_calender(calender):
                     date = event_date.text
                 event_times = event.findAll("td", class_="event-time")
                 if event_times:
-                    times = [event_time.text if event_time != ' ' else 'All day' for event_time in event_times]
+                    times = [event_time.text if event_time.text != '' else 'All day' for event_time in event_times]
                     if is_british_summer_time():
-                        times = [(datetime.strptime(time, TIME_FORMAT) + timedelta(hours=1)).strftime(format=TIME_FORMAT)
-                                if time != ' ' else 'All day' for time in times]
+                        times = [(datetime.strptime(time, TIME_FORMAT) + timedelta(hours=1)).strftime(format=TIME_FORMAT) if time != 'All day' else 'All day' for time in times ]
                 event_summary = event.findAll("div", class_="event-summary")
                 if event_summary:
                     descriptions = [event.text for event in event_summary]
