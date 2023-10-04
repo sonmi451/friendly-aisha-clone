65f0681841b332dc0cb280e4c94193260ec44176
Aisha <aishamclean451@gmail.com>
2023-10-04 22:47:50 +0100

    remove tv games time and place response

--- robot/robot.py
+++ robot/robot.py
@@ -411,16 +411,10 @@ async def on_message(message):
         response = embed_guess_the_soup_rules()
         await message.channel.send(embed=response)
 
-    if 'tv' in chat_message and 'game' in chat_message and 'help' in chat_message:
+    if 'tv' in chat_message and 'game' in chat_message:
         tv_games_help = get_random(TV_GAMES_HELP)
         response = embed_response(tv_games_help)
         await message.channel.send(embed=response)
-
-    if 'tv games' in chat_message:
-        schedule = None
-        #schedule = await scrape_events_from_calender(TV_GAMES_AGENDA)
-        print_schedule = embed_games_schedule(schedule)
-        await message.channel.send(embed=print_schedule)
 
     if 'movie schedule' in chat_message:
         # schedule = await scrape_events_from_calender(MOVIE_AGENDA)
